7e450f2ecc6e7aac7baad162a2a6a6ac711d8717
Felipe A. Díaz <studio.pipe.95@gmail.com>
2022-01-21 21:47:45 -0300

    adding "!cm" msg command

--- src/main.rs
+++ src/main.rs
@@ -36,35 +36,43 @@ async fn main() -> irc::error::Result<()> {
         match message.command {
             Command::PRIVMSG(_, ref msg) => {
 
-                let user_nickname = if let Some(irc::client::prelude::Prefix::Nickname(nick, _, _)) = message.prefix {
-                    nick
-                }       else {
-                    "noname".to_owned()
-                };
+                if msg.len() > 4 {
 
-                //create a TwitchMsg and push it to the Vec<TwitchMsg>
-                twitch_msg.push( TwitchMsg::new( user_nickname.to_owned(), msg.to_owned() ) );
-                //print Msg vector to screen
-                println!("{:#?}", &twitch_msg);
+                    if &msg[0..=3] == "!cm " {
 
-                //generate a JSON String
-                let json = serde_json::to_string(&TwitchMsg::new(user_nickname.to_owned(), msg.to_owned()));
+                        let user_nickname = if let Some(irc::client::prelude::Prefix::Nickname(nick, _, _)) = message.prefix {
+                            nick
+                        }       else {
+                            "noname".to_owned()
+                        };
 
-                counter += 1;
+                        //create a TwitchMsg and push it to the Vec<TwitchMsg>
+                        twitch_msg.push( TwitchMsg::new( user_nickname.to_owned(), msg[4..].to_owned() ) );
+                        //print Msg vector to screen
+                        println!("{:#?}", &twitch_msg);
 
-                //manage json error
-                match json {
-                    Ok(json_string) => {
-                        //format file name
-                        let file_name = format!("output/{counter:0>7}.json");
-                        //print json to screen
-                        println!("file: {file_name} -> {json_string:#?}");
-                        //save JSON file
-                        let mut json_file = File::create(Path::new(&file_name))?;
-                        json_file.write_all(&json_string.as_bytes())?;
+                        //generate a JSON String
+                        let json = serde_json::to_string(&TwitchMsg::new(user_nickname.to_owned(), msg[4..].to_owned()));
+
+                        counter += 1;
+
+                        //manage json error
+                        match json {
+                            Ok(json_string) => {
+                                //format file name
+                                let file_name = format!("output/{counter:0>7}.json");
+                                //print json to screen
+                                println!("file: {file_name} -> {json_string:#?}");
+                                //save JSON file
+                                let mut json_file = File::create(Path::new(&file_name))?;
+                                json_file.write_all(&json_string.as_bytes())?;
+                            }
+
+                            _ => (),
+                        }
+
                     }
 
-                    _ => (),
                 }
 
             }
